3b5f6b76f7f7b111f358b80880d2f3c60b0bff02
rycha <rycha@example.com>
2026-06-03 10:42:58 -0300

    fix: muda base retangular do suporte 2p para a vertical

--- configurador.js
+++ configurador.js
@@ -162,8 +162,8 @@ function rebuildGeometries() {
             base = new THREE.Mesh(baseGeo, material);
             base.rotation.x = Math.PI / 2;
         } else {
-            // Suporte 2p (2 furos) com base retangular horizontal baseada na foto
-            baseGeo = new THREE.BoxGeometry(0.06, 0.025, 0.015);
+            // Suporte 2p (2 furos) com base retangular vertical baseada na foto
+            baseGeo = new THREE.BoxGeometry(0.025, 0.06, 0.015);
             base = new THREE.Mesh(baseGeo, material);
         }
         
